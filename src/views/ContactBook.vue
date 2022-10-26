212de6429552cab4d89aeb8aa2a03c086f3b66b5
b1910395 <khoib1910395@student.ctu.edu.vn>
2022-10-26 23:34:00 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -8,17 +8,26 @@
                 Danh bạ
                 <i class="fas fa-address-book"></i>
             </h4>
-            <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts"
-                v-model:activeIndex="activeIndex" />
+            <ContactList 
+                v-if="filteredContactsCount > 0" 
+                :contacts="filteredContacts"
+                v-model:activeIndex="activeIndex" 
+            />
             <p v-else>Không có liên hệ nào.</p>
+
             <div class="mt-3 row justify-content-around align-items-center">
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
+
                 <button class="btn btn-sm btn-success" @click="goToAddContact">
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
-                <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+
+                <button 
+                    class="btn btn-sm btn-danger" 
+                    @click="removeAllContacts"
+                >
                     <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
             </div>
@@ -30,10 +39,21 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh
+                    </span>
+                </router-link>
             </div>
         </div>
     </div>
 </template>
+
 <script>
 import ContactCard from "@/components/ContactCard.vue";
 import InputSearch from "@/components/InputSearch.vue";
@@ -44,7 +64,8 @@ export default {
         ContactCard,
         InputSearch,
         ContactList,
-    },// Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
+    },
+    // Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
     data() {
         return {
             contacts: [],
@@ -71,7 +92,8 @@ export default {
         filteredContacts() {
             if (!this.searchText) return this.contacts;
             return this.contacts.filter((_contact, index) =>
-                this.contactStrings[index].includes(this.searchText));
+                this.contactStrings[index].includes(this.searchText)
+            );
         },
         activeContact() {
             if (this.activeIndex < 0) return null;
